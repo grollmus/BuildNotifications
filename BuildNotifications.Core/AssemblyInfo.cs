cb259cb3918299a119a0a215317d991e876a927f
Matthias Specht <Matthias.Specht@grollmus.de>
2019-06-12 13:29:35 +0200

    Reduce noise in log messages in core

--- BuildNotifications.Core/AssemblyInfo.cs
+++ BuildNotifications.Core/AssemblyInfo.cs
@@ -1,4 +1,7 @@
 ﻿using System.Runtime.CompilerServices;
+using Anotar.NLog;
+
+[assembly: LogMinimalMessage]
 
 [assembly: InternalsVisibleTo("BuildNotifications.Core.Tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
