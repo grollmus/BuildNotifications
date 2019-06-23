18cd4a0e25b4a957da28ec989b2d14f5f4eca119
Bernd N. Krupinski <BerndKrupinski@gmail.com>
2019-06-23 21:21:58 +0200

    Create Icon Control "IconBatch" vor XAML icons

---
+++ BuildNotifications/Resources/Icons/IconBatch.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace BuildNotifications.Resources.Icons
+{
+    /// <summary>
+    /// Interaction logic for IconBatch.xaml
+    /// </summary>
+    public partial class IconBatch : UserControl
+    {
+        public static readonly DependencyProperty ForegroundProperty = DependencyProperty.Register(
+            "Foreground", typeof(Brush), typeof(IconBatch), new PropertyMetadata(default(Brush)));
+
+        public Brush Foreground
+        {
+            get => (Brush) GetValue(ForegroundProperty);
+            set => SetValue(ForegroundProperty, value);
+        }
+
+        public static readonly DependencyProperty TypeProperty = DependencyProperty.Register(
+            "Type", typeof(IconType), typeof(IconBatch), new PropertyMetadata(default(IconType)));
+
+        public IconType Type
+        {
+            get => (IconType) GetValue(TypeProperty);
+            set => SetValue(TypeProperty, value);
+        }
+
+        public IconBatch()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public enum IconType
+    {
+        Connection,
+        Branch,
+        Definition
+    }
+}
