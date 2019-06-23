29318f365f25d56a6d7eec26d5c23c3c050d4952
Bernd N. Krupinski <BerndKrupinski@gmail.com>
2019-06-23 23:06:46 +0200

    Further style and animate TitleBar

---
+++ BuildNotifications/Resources/Animation/WidthChange.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Interactivity;
+using TweenSharp.Animation;
+using TweenSharp.Factory;
+
+namespace BuildNotifications.Resources.Animation
+{
+    internal class WidthChange : TriggerAction<FrameworkElement>
+    {
+        public static readonly DependencyProperty TargetWidthProperty = DependencyProperty.Register(
+            "TargetWidth", typeof(double), typeof(WidthChange), new PropertyMetadata(default(double)));
+
+        public double TargetWidth
+        {
+            get => (double) GetValue(TargetWidthProperty);
+            set => SetValue(TargetWidthProperty, value);
+        }
+
+        public double Duration { get; set; } = 0.35;
+
+        protected override void Invoke(object parameter)
+        {
+            var globalTweenHandler = App.GlobalTweenHandler;
+            globalTweenHandler.ClearTweensOf(AssociatedObject);
+
+            globalTweenHandler.Add(AssociatedObject.Tween(x => x.Width).To(TargetWidth).In(Duration).Ease(Easing.QuadraticEaseOut));
+        }
+    }
+}
