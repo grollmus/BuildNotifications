50b9fdba3da88c8f95178821351033cd9db8b0d9
Matthias Specht <Matthias.Specht@grollmus.de>
2019-08-23 07:07:37 +0200

    Add SynchronizationContextRemover

---
+++ BuildNotifications.Core/SynchronizationContextRemover.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace BuildNotifications.Core
+{
+    public class SynchronizationContextRemover : INotifyCompletion
+    {
+        public bool IsCompleted => SynchronizationContext.Current == null;
+
+        public SynchronizationContextRemover GetAwaiter()
+        {
+            return this;
+        }
+
+        public void GetResult()
+        {
+        }
+
+        /// <inheritdoc />
+        public void OnCompleted(Action continuation)
+        {
+            var prevContext = SynchronizationContext.Current;
+            try
+            {
+                SynchronizationContext.SetSynchronizationContext(null);
+                continuation();
+            }
+            finally
+            {
+                SynchronizationContext.SetSynchronizationContext(prevContext);
+            }
+        }
+    }
+}
