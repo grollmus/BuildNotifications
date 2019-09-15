7ba6e18609b6788ea5e55e4a0a6557b7d4814ff9
Bernd N. Krupinski <BerndKrupinski@gmail.com>
2019-09-15 16:14:13 +0200

    Add base for fetching values asynchronously in TfsConfiguration. Part of #40

--- Plugins/BuildNotifications.Plugin.Tfs/TfsConfiguration.cs
+++ Plugins/BuildNotifications.Plugin.Tfs/TfsConfiguration.cs
@@ -1,4 +1,9 @@
-﻿using System.Security;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Threading.Tasks;
+using Anotar.NLog;
 using JetBrains.Annotations;
 using ReflectSettings.Attributes;
 
@@ -11,6 +16,7 @@ namespace BuildNotifications.Plugin.Tfs
 
         public string? CollectionName { get; set; }
 
+        [CalculatedValuesAsync(nameof(FetchProjectIds))]
         public string? ProjectId { get; set; }
 
         public string? RepositoryId { get; set; }
@@ -34,6 +40,56 @@ namespace BuildNotifications.Plugin.Tfs
         public bool PasswordHidden() => AuthenticationType != AuthenticationType.Account;
 
         public bool TokenHidden() => AuthenticationType != AuthenticationType.Token;
+
+        private string? _lastFetchedUrl;
+        private IEnumerable<object> _lastFetchResult = Enumerable.Empty<object>();
+
+        public async Task<IEnumerable<object>> FetchProjectIds()
+        {
+            if (_lastFetchedUrl == Url || Url == null)
+            {
+                if (_lastFetchResult != null)
+                    return _lastFetchResult;
+
+                if (ProjectId != null)
+                    return new List<string> {ProjectId};
+                else
+                    return Enumerable.Empty<string>();
+            }
+
+            var urlToFetch = Url;
+
+            var result = await FetchProjectIdsInternal(urlToFetch);
+            _lastFetchResult = result.ToList();
+
+            return _lastFetchResult;
+        }
+
+        private async Task<IEnumerable<object>> FetchProjectIdsInternal(string urlToFetch)
+        {
+            try
+            {
+                // do some fetch logic
+                await Task.Delay(1000);
+                if (Url == "SomeWorkingUrl")
+                    return new List<string> {"ID1", "ID2", "ID3"};
+                else
+                    throw new Exception("Hmm");
+                //return Enumerable.Empty<string>();
+            }
+            catch (Exception e)
+            {
+                LogTo.InfoException("Failed to fetch projectIds", e);
+                if (ProjectId != null)
+                    return new List<object> {ProjectId};
+                else
+                    return Enumerable.Empty<object>();
+            }
+            finally
+            {
+                _lastFetchedUrl = urlToFetch;
+            }
+        }
     }
 
     public enum AuthenticationType
