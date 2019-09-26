b03f7d749730ec64bafa94e936e0137edf0e1818
Matthias Specht <sylence@btbsoft.org>
2019-09-26 21:46:45 +0200

    Only download latest release if there is one
    Part of #18

--- Scripts/create-release-package.ps1
+++ Scripts/create-release-package.ps1
@@ -9,37 +9,40 @@ $nugetUrl = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
 ###############################################################################
 New-Item -ItemType Directory -Force -Path $targetFolder
 
-Write-Host Determining latest release
+Write-Output Determining latest release
 $latestReleaseUrl = "https://api.github.com/repos/$repo/releases"
 $tag = (Invoke-WebRequest $latestReleaseUrl | ConvertFrom-Json)[0].tag_name
-$version = $tag.Substring(1)
-Write-Host Latest release is $tag => $version
+if($tag)
+{
+    $version = $tag.Substring(1)
+    Write-Output Latest release is $tag => $version
 
-Write-Host Downloading latest RELEASE file
-$releasesFileUrl = "https://github.com/$repo/releases/download/$tag/RELEASES";
-$releasesFilePath = "$targetFolder/RELEASES"
-Invoke-WebRequest $releasesFileUrl -Out $releasesFilePath
+    Write-Output Downloading latest RELEASE file
+    $releasesFileUrl = "https://github.com/$repo/releases/download/$tag/RELEASES";
+    $releasesFilePath = "$targetFolder/RELEASES"
+    Invoke-WebRequest $releasesFileUrl -Out $releasesFilePath
 
-Write-Host Downloading latest full package
-$fullPackageFileName = "$applicationName-$version-full.nupkg"
-$fullPackageUrl = "https://github.com/$repo/releases/download/$tag/$fullPackageFileName"
-$fullPackageFilePath = "$targetFolder/$fullPackageFileName"
-Invoke-WebRequest $fullPackageUrl -Out $fullPackageFilePath
+    Write-Output Downloading latest full package
+    $fullPackageFileName = "$applicationName-$version-full.nupkg"
+    $fullPackageUrl = "https://github.com/$repo/releases/download/$tag/$fullPackageFileName"
+    $fullPackageFilePath = "$targetFolder/$fullPackageFileName"
+    Invoke-WebRequest $fullPackageUrl -Out $fullPackageFilePath
+}
 
-Write-Host Downloading squirrel
+Write-Output Downloading squirrel
 $squirrelZipFile = "Squirrel.zip"
 Invoke-WebRequest $squirrelUrl -Out $squirrelZipFile
 Expand-Archive $squirrelZipFile -Force -DestinationPath .
 
-Write-Host Downloading nuget.exe
+Write-Output Downloading nuget.exe
 Invoke-WebRequest $nugetUrl -Out "nuget.exe"
 
-Write-Host Creating nuget package
+Write-Output Creating nuget package
 $nuspecFileName = "Scripts/$applicationName.nuspec" 
 $nupkgFileName = "$applicationName-$versionToBuild.nupkg"
 .\nuget.exe pack $nuspecFileName -Version $versionToBuild
 
-Write-Host Creating squirrel release
+Write-Output Creating squirrel release
 .\squirrel.exe --releasify $nupkgFileName
 
-Write-Host Done
+Write-Output Done
