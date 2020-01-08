b3430ac16834176b87aa14c922878cb3b96d66d5
Matthias Specht <Matthias.Specht@grollmus.de>
2020-01-08 10:34:24 +0100

    Display current version in info dialog
    Closes #113

---
+++ BuildNotifications/ViewModel/Utils/NullToVisibilityConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace BuildNotifications.ViewModel.Utils
+{
+    internal class NullToVisibilityConverter : IValueConverter
+    {
+        private NullToVisibilityConverter(bool inverted)
+        {
+            _inverted = inverted;
+        }
+
+        public static NullToVisibilityConverter Instance { get; } = new NullToVisibilityConverter(false);
+        public static NullToVisibilityConverter Inverted { get; } = new NullToVisibilityConverter(true);
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return _inverted ? Visibility.Visible : Visibility.Collapsed;
+
+            return _inverted ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        private readonly bool _inverted;
+    }
+}
--- BuildNotifications/Views/InfoPopupDialog.xaml.cs
+++ BuildNotifications/Views/InfoPopupDialog.xaml.cs
@@ -1,12 +1,9 @@
-﻿using BuildNotifications.ViewModel;
-
-namespace BuildNotifications.Views
+﻿namespace BuildNotifications.Views
 {
     public partial class InfoPopupDialog
     {
         public InfoPopupDialog()
         {
-            DataContext = new InfoPopupViewModel();
             InitializeComponent();
         }
     }
