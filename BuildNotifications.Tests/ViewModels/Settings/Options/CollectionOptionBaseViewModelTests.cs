572a63780dd48af38a827b8a20150cad7e7ca3eb
Matthias Specht <matthias.specht@grollmus.de>
2020-09-16 14:22:50 +0200

    Improve test coverage

--- BuildNotifications.Tests/ViewModels/Settings/Options/CollectionOptionBaseViewModelTests.cs
+++ BuildNotifications.Tests/ViewModels/Settings/Options/CollectionOptionBaseViewModelTests.cs
@@ -1,4 +1,5 @@
-﻿using BuildNotifications.ViewModel.Settings.Options;
+﻿using System;
+using BuildNotifications.ViewModel.Settings.Options;
 using Xunit;
 
 namespace BuildNotifications.Tests.ViewModels.Settings.Options
@@ -8,13 +9,35 @@ namespace BuildNotifications.Tests.ViewModels.Settings.Options
         private class CollectionOptionViewModel : CollectionOptionBaseViewModel<int, NumberOptionViewModel>
         {
             public CollectionOptionViewModel()
-                : base(System.Array.Empty<int>(), string.Empty)
+                : base(Array.Empty<int>(), string.Empty)
             {
             }
 
-            protected override NumberOptionViewModel CreateNewValue() => CreateNewValue(0);
+            protected override NumberOptionViewModel CreateNewValue()
+            {
+                return CreateNewValue(0);
+            }
 
-            protected override NumberOptionViewModel CreateNewValue(int value) => new NumberOptionViewModel(value, 0, 10, string.Empty);
+            protected override NumberOptionViewModel CreateNewValue(int value)
+            {
+                return new NumberOptionViewModel(value, 0, 10, string.Empty);
+            }
+        }
+
+        private class CollectionOptionViewModelWithDisabledAdd : CollectionOptionViewModel
+        {
+            protected override bool CanAddNewItem()
+            {
+                return false;
+            }
+        }
+
+        private class CollectionOptionViewModelWithDisabledRemove : CollectionOptionViewModel
+        {
+            protected override bool CanRemoveItem(int value)
+            {
+                return false;
+            }
         }
 
         [Fact]
@@ -28,6 +51,58 @@ namespace BuildNotifications.Tests.ViewModels.Settings.Options
 
             // Assert
             Assert.Single(sut.Values);
+        }
+
+        [Fact]
+        public void AddNewItemCommandShouldBeDisabledWhenCanAddReturnsFalse()
+        {
+            // Arrange
+            var sut = new CollectionOptionViewModelWithDisabledAdd();
+
+            // Act
+            var actual = sut.AddNewItemCommand.CanExecute(null);
+
+            // Assert
+            Assert.False(actual);
+        }
+
+        [Fact]
+        public void AddNewItemCommandShouldBeEnabledWhenNotOverriden()
+        {
+            // Arrange
+            var sut = new CollectionOptionViewModel();
+
+            // Act
+            var actual = sut.AddNewItemCommand.CanExecute(null);
+
+            // Assert
+            Assert.True(actual);
+        }
+
+        [Fact]
+        public void RemoveItemCommandShouldBeDisabledWhenCanRemoveReturnsFalse()
+        {
+            // Arrange
+            var sut = new CollectionOptionViewModelWithDisabledRemove();
+
+            // Act
+            var actual = sut.RemoveItemCommand.CanExecute(0);
+
+            // Assert
+            Assert.False(actual);
+        }
+
+        [Fact]
+        public void RemoveItemCommandShouldBeEnabledWhenNotOverriden()
+        {
+            // Arrange
+            var sut = new CollectionOptionViewModel();
+
+            // Act
+            var actual = sut.RemoveItemCommand.CanExecute(0);
+
+            // Assert
+            Assert.True(actual);
         }
 
         [Fact]
---
+++ BuildNotifications.Tests/ViewModels/Settings/Options/NumberOptionViewModelTests.cs
@@ -0,0 +1,23 @@
+﻿using BuildNotifications.ViewModel.Settings.Options;
+using Xunit;
+
+namespace BuildNotifications.Tests.ViewModels.Settings.Options
+{
+    public class NumberOptionViewModelTests
+    {
+        [Fact]
+        public void ConstructorShouldSetCorrectMinMaxValues()
+        {
+            // Arrange
+            const int expectedMin = 5;
+            const int expectedMax = 12;
+
+            // Act
+            var actual = new NumberOptionViewModel(10, expectedMin, expectedMax, string.Empty);
+
+            // Assert
+            Assert.Equal(expectedMin, actual.MinValue);
+            Assert.Equal(expectedMax, actual.MaxValue);
+        }
+    }
+}
