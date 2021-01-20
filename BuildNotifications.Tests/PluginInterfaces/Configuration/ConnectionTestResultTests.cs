508a3b75b7954dd7784f0cf103218b169531391d
Matthias Specht <matthias.specht@grollmus.de>
2021-01-20 07:21:42 +0100

    Add some tests

---
+++ BuildNotifications.Tests/PluginInterfaces/Configuration/ConnectionTestResultTests.cs
@@ -0,0 +1,53 @@
+﻿using BuildNotifications.PluginInterfaces;
+using Xunit;
+
+namespace BuildNotifications.Tests.PluginInterfaces.Configuration
+{
+    public class ConnectionTestResultTests
+    {
+        [Fact]
+        public void FailureShouldContainMultipleMessages()
+        {
+            // Arrange
+            var allMessages = new[]
+            {
+                "message-1",
+                "error-2",
+                "message-3"
+            };
+            var sut = ConnectionTestResult.Failure(allMessages);
+
+            // Assert
+            Assert.False(sut.IsSuccess);
+            Assert.Equal(allMessages, sut.Errors);
+            Assert.Contains(allMessages[0], sut.ErrorMessage);
+            Assert.Contains(allMessages[1], sut.ErrorMessage);
+            Assert.Contains(allMessages[2], sut.ErrorMessage);
+        }
+
+        [Fact]
+        public void FailureShouldContainSingleMessage()
+        {
+            // Arrange
+            const string message = "test-error";
+            var sut = ConnectionTestResult.Failure(message);
+
+            // Assert
+            Assert.False(sut.IsSuccess);
+            Assert.Contains(message, sut.ErrorMessage);
+            Assert.Contains(message, sut.Errors);
+        }
+
+        [Fact]
+        public void SuccessShouldNotContainAnyErrors()
+        {
+            // Arrange
+            var sut = ConnectionTestResult.Success;
+
+            // Assert
+            Assert.True(sut.IsSuccess);
+            Assert.Empty(sut.ErrorMessage);
+            Assert.Empty(sut.Errors);
+        }
+    }
+}
---
+++ BuildNotifications.Tests/PluginInterfaces/Configuration/Options/ListOptionItemTests.cs
@@ -0,0 +1,47 @@
+﻿using BuildNotifications.PluginInterfaces.Configuration.Options;
+using Xunit;
+
+namespace BuildNotifications.Tests.PluginInterfaces.Configuration.Options
+{
+    public class ListOptionItemTests
+    {
+        private class TestListOptionItem : ListOptionItem<int>
+        {
+            public TestListOptionItem(int value = 0, string displayName = null, bool localize = false) : base(value, displayName ?? string.Empty, localize)
+            {
+            }
+        }
+
+        [Fact]
+        public void DisplayNameShouldBeUsedFromConstructor()
+        {
+            // Arrange
+            const string displayName = "display-name";
+            var sut = new TestListOptionItem(0, displayName);
+
+            // Act
+            var actualImplementation = sut.DisplayName;
+            var actualInterface = ((IListOptionItem) sut).DisplayName;
+
+            // Assert
+            Assert.Equal(displayName, actualInterface);
+            Assert.Equal(displayName, actualImplementation);
+        }
+
+        [Fact]
+        public void ValueShouldBeUsedFromConstructor()
+        {
+            // Arrange
+            const int value = 123;
+            var sut = new TestListOptionItem(value);
+
+            // Act
+            var actualImplementation = sut.Value;
+            var actualInterface = ((IListOptionItem) sut).Value;
+
+            // Assert
+            Assert.Equal(value, actualInterface);
+            Assert.Equal(value, actualImplementation);
+        }
+    }
+}
