a027147e8a830e27190aacfcfeae99664618995b
Matthias Specht <matthias.specht@grollmus.de>
2022-07-06 08:26:31 +0200

    Fix failing tests

--- Plugins/BuildNotifications.Plugin.Tfs/Resources/Strings.en.Designer.cs
+++ Plugins/BuildNotifications.Plugin.Tfs/Resources/Strings.en.Designer.cs
@@ -39,7 +39,7 @@ namespace BuildNotifications.Plugin.Tfs.Resources {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BuildNotifications.Plugin.Tfs.Resources.Strings.en", typeof(Strings).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BuildNotifications.Plugin.Tfs.Resources.Strings", typeof(Strings).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
